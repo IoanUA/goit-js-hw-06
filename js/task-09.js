549bad7c40fa2ec17c5d07b533bcea550c0426b1
Chuguev_Ivan <Chuguev19930612@gmail.com>
2023-11-20 23:30:04 +0200

    p

--- js/task-09.js
+++ js/task-09.js
@@ -2,18 +2,17 @@ const body = document.querySelector('body');
 const butt = document.querySelector('.change-color')
 const span = document.querySelector('.color')
 
-butt.addEventListener('click', onChangeBackground)
 
 
 function getRandomHexColor() {
- 
  return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-
 }
 
-function onChangeBackground(){
-  span.textContent = getRandomHexColor();
-  body.style.backgroundColor = getRandomHexColor();
-}
+butt.addEventListener("click", function () {
+  var randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
+
